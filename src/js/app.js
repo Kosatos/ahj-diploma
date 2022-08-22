54a6511380242e57ed8c71069afbdeb3e37dd295
Konstantin Muzyka <kosatkinms@mail.ru>
2022-08-23 01:23:32 +0300

    interface done

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+  document.querySelector('textarea').addEventListener('input', function (e) {
+    e.target.style.height = 'auto';
+    e.target.style.height = e.target.scrollHeight + 2 + 'px';
+  });
+};
